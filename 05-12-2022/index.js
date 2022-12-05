ea5993212a898c2783caf759dd4f6e36be4ea522
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2022-12-05 13:44:57 +0500

    05-12-2022

---
+++ 05-12-2022/index.js
@@ -0,0 +1,52 @@
+// These variable are storing name and age
+
+// var name="John"
+// var age=23
+
+var number1=prompt("enter first number")   
+var number2=prompt("Enter Second Number")  
+var add=parseFloat(number1)+parseFloat(number2)
+document.write("Addition Result: "+add)
+var subtract=parseFloat(number1)-parseFloat(number2)
+document.write("Subtraction Result: "+subtract)
+// var multiply=parseFloat(number1)*parseFloat(number2)
+// alert(multiply)
+// var divide=parseFloat(number1)/parseFloat(number2)
+// alert(divide.toFixed(4))
+
+//Area of circle
+// var pi=3.142
+// var radius=prompt("Enter Radius of Circle")
+// var areaofcircle=parseFloat(pi)*(parseFloat(radius)*parseFloat(radius))
+// alert(areaofcircle)
+
+//Area of Triangle
+// var base=prompt("Enter base of triangle")
+// var height=prompt("Enter height of triangle")
+// var areaoftriangle=(1/2)*(parseFloat(base)*parseFloat(height))
+// alert("The area of triangle is: "+areaoftriangle)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var result=number1+number2
+// alert(result)
+
+
+
+
